b51bac4a49b505b8ef480575c398684a5d96f370
haeun01 <cjswldhkdpf@naver.com>
2024-03-04 11:19:26 +0900

    forControl

--- java/src/Java240304.java
+++ java/src/Java240304.java
@@ -1,14 +1,54 @@
 public class Java240304 {
     public static void main(String[] args) {
-        // for 반복문의 기본형
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-
+        // for 반복문의 기본문
+        int a;
+        for(a = 0; a < 3; a++) {
+            System.out.print(a + " ");
         }
-        //무한루프
-        //조건식이 false가 될 수 있는지 여부를 반드시 확인!!
-        /* for (int i = 0; i >= 0; i--) {
-            System.out.println(i);
-        } */
+        System.out.println();
+
+        for(int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for(int i = 0; i < 10; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for(int i = 0, j = 0; i < 10; i++, j++) {
+            System.out.print(i + j  + " ");
+        }
+        System.out.println();
+
+        // for 문의 특수한 형태(무한 루프)
+        /*
+        for(int i = 0;  ; i++) {
+                System.out.println(i + " ");
+                }
+                for(;;) {
+                System.out.println("무한 루프");
+                }
+         */
+
+        //무한 루프 탈출
+        for(int i = 0; ; i++) {
+            if(i > 10) {
+                break;
+            }
+            System.out.print(i + " ");
+        }
+        System.out.println("무한 루프 탈출");
     }
 }
