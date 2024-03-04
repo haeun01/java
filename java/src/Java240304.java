893d9730e058ae90241bb0a838d5c44d9167297f
haeun01 <cjswldhkdpf@naver.com>
2024-03-04 12:01:56 +0900

    roof

--- java/src/Java240304.java
+++ java/src/Java240304.java
@@ -1,8 +1,7 @@
 public class Java240304 {
     public static void main(String[] args) {
         // for 반복문의 기본문
-        int a;
-        for(a = 0; a < 3; a++) {
+        for(int a = 0; a < 3; a++) {
             System.out.print(a + " ");
         }
         System.out.println();
@@ -50,5 +49,24 @@ public class Java240304 {
             System.out.print(i + " ");
         }
         System.out.println("무한 루프 탈출");
+
+        //for 반복문 변형하기
+        int i = 0;
+        for ( ; ; ) {
+            if (i < 5) {
+                System.out.print(i + " ");
+                i++;
+            } else {
+                break;
+            }
+        }
+        System.out.println();
+
+        // 멀티 조건식
+        for (int a = 0, b = 0; a < 5 || b < 5; a++, b++) {
+            System.out.print(a + "_" + b + " ");
+        }
     }
+
+
 }
