df4509b13a321d79987979bb329aef7ae3262602
haeun01 <cjswldhkdpf@naver.com>
2024-03-04 15:37:48 +0900

    Array commit

--- java/src/Array.java
+++ java/src/Array.java
@@ -13,13 +13,33 @@ public class Array {
         intArray2[4] = 5;
         System.out.println(intArray2[4]);
 
-        int[] intArray3 = new int[5];
-        for (int i = 0; i < 5 ; i++) {
+        int[] intArray3 = new int[50];
+        // int[] intArray3 = 가변의 길이를 가진 배열을 저장함
+        for (int i = 0; i < intArray3.length ; i++) {
             intArray3[i] = i+1;
             System.out.print(intArray3[i] + " ");
         }
         System.out.println();
 
-        
+        int[] intArray4 = new int[]{1,2,3,4,5};
+        System.out.println(intArray4[0]);
+
+        //배열의 복사 ( = 참조복사 )
+        String[] str1 = {"black", "red", "blue"};
+        String[] str2 = str1;                //같은 메모리 주소를 공유(메모리 절약)
+        System.out.println("1 " + str2[0]);
+        str2[0] = "white";                   //이름 바꾸기
+        System.out.println("2 " + str2[0]);
+        System.out.println("3 " + str1[0]);  //이름이 바꾸어져 있는 상태로 결과값
+        // 기본 자료형의 복사 ( = 값 복사 )
+        int a = 3;
+        int b = a;
+        System.out.println("1. " + b);
+        b = 6;
+        System.out.println("2. " + b);
+        System.out.println("3. " + a); //값 자체를 가지고 있음
+
+        //메모리가 가지고 있는 값이 다르다는 차이점이 있다.
+        //stack의 메모리는 공유하지 않는다.
     }
 }
