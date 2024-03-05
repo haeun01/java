5ba2ac86e61e35b1b7b3f557cd00d31cdaa53ac0
haeun01 <cjswldhkdpf@naver.com>
2024-03-05 12:37:38 +0900

    length

--- java/src/Array0305.java
+++ java/src/Array0305.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Array0305 {
     public static void main(String[] args) {
         //배열 선언법
@@ -23,6 +25,7 @@ public class Array0305 {
             System.out.println(intArray4[i] + " ");
         }
 
+        // 배열의 값 대입 방법 1
         int[] array1 = new int[3];
         array1[0] = 3;
         array1[1] = 4;
@@ -36,6 +39,7 @@ public class Array0305 {
         array2[2] = 5;
         System.out.println(array2[0] + " " + array2[1] + " " + array2[2]);
 
+        //배열의 값 대입 방법 2
         int[] array3 = new int[]{3,4,5};
         System.out.println(array3[0] + " " + array3[1] + " " + array3[2]);
 
@@ -43,9 +47,21 @@ public class Array0305 {
         array4 = new int[]{3,4,5};
         System.out.println(array4[0] + " " + array4[1] + " " + array4[2]);
 
+        //배열의 값 대입 방법 3
         int[] array5 = {3,4,5};
         System.out.println(array5[0] + " " + array5[1] + " " + array5[2]);
 
-
+        //배열의 초기화
+        //배열을 선언, 생성하고 입력값을 넣지 않은 경우, 자동으로 초기 값이 입력됨
+        int[] intList = new int[3];
+        float[] floatList = new float[3];
+        boolean[] boolList = new boolean[3];
+        char[] charList = new char[3];
+        String[] strList = new String[3];
+        System.out.println(Arrays.toString(intList));
+        System.out.println(Arrays.toString(floatList));
+        System.out.println(Arrays.toString(boolList));
+        System.out.println(Arrays.toString(charList));
+        System.out.println(Arrays.toString(strList));
     }
 }
---
+++ java/src/Array03052.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Array03052 {
+    public static void main(String[] args) {
+        // 기본 자료형의 복사  ==> 값 복사
+        int a = 3;
+        int b = a;
+        System.out.println(a +  " " + b); // 3, 3
+        b = 5;
+        System.out.println(a + " " + b); // 3, 5
+
+        // 참조자료형의 복사 ==> 참조 복사 또는 주소 복사
+        int[] intList1 = {3,4,5};
+        int[] intList2 = intList1;
+        System.out.println(Arrays.toString(intList2));
+        intList2[0] = 10;
+        System.out.println(Arrays.toString(intList2));
+        System.out.println(Arrays.toString(intList1));
+
+        //배열과 for문의 사용
+        //조건식에 배열의 갯수(length)를 사용함
+        for (int i = 0; i<intList2.length ; i++) {
+            System.out.println(intList2[i]);
+        }
+    }
+}
