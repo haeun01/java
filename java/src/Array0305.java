89f0afeeb83cc3668d74e024ed9346f4e112ae27
haeun01 <cjswldhkdpf@naver.com>
2024-03-05 11:28:26 +0900

    Array

--- java/src/Array0305.java
+++ java/src/Array0305.java
@@ -15,5 +15,37 @@ public class Array0305 {
         for (int i = 0; i < 5; i++) {
             System.out.print(intArray3[i] + ", ");
         }
+        System.out.println();
+
+        // 배열의 선언, 생성, 입력을 한 번에 수행하는 코드
+        int[] intArray4 = {10,20,30,40,50};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(intArray4[i] + " ");
+        }
+
+        int[] array1 = new int[3];
+        array1[0] = 3;
+        array1[1] = 4;
+        array1[2] = 5;
+        System.out.println(array1[0] + " " + array1[1] + " " + array1[2]);
+
+        int[] array2;
+        array2 = new int[3];
+        array2[0] = 3;
+        array2[1] = 4;
+        array2[2] = 5;
+        System.out.println(array2[0] + " " + array2[1] + " " + array2[2]);
+
+        int[] array3 = new int[]{3,4,5};
+        System.out.println(array3[0] + " " + array3[1] + " " + array3[2]);
+
+        int[] array4;
+        array4 = new int[]{3,4,5};
+        System.out.println(array4[0] + " " + array4[1] + " " + array4[2]);
+
+        int[] array5 = {3,4,5};
+        System.out.println(array5[0] + " " + array5[1] + " " + array5[2]);
+
+
     }
 }
