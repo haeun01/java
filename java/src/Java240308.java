d87ee0d2ba76e653a352d98dadd7c807c20f1317
haeun01 <cjswldhkdpf@naver.com>
2024-03-08 10:17:00 +0900

    student

---
+++ java/src/Java240308.java
@@ -0,0 +1,5 @@
+public class Java240308 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java/src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    String name;
+    int age;
+    String address;
+
+    public Student() { //기본 생성자
+
+    }
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}
