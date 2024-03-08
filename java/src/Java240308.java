c74fd214c93917c1136daec84389bf7eb65b66ae
haeun01 <cjswldhkdpf@naver.com>
2024-03-08 14:41:31 +0900

    class

--- java/src/Java240308.java
+++ java/src/Java240308.java
@@ -1,5 +1,35 @@
 public class Java240308 {
     public static void main(String[] args) {
+        Student tom = new Student("Tom", 18, "Daejeon", "123456");
+        Student steve = new Student("Steve", 25, "Seoul","246810");
+        Student paul = new Student();
+        paul.setName("Paul");
+        paul.setAge(22);
+        paul.setAddress("Incheon");
+        tom.displayAll();
+        steve.displayAll();
+        paul.displayAll();
 
+        System.out.println(tom.getPersonalNum());
+
+        // 메소드 호출
+        int[] numberList = {12, 34, 56, 32, 84, 99, 73,69};
+        int[] numberList2 = {123, 345, 235, 7898, 467};
+        getSumAvg(numberList);
+        getSumAvg(numberList2);
+    }
+
+    static void  getSumAvg(int[] numberList) {
+
+
+        //합계
+        int sum = 0;
+        for(int i = 0; i < numberList.length; i++) {
+            sum = sum + numberList[i];
+        }
+
+        //평균
+        int average = sum / numberList.length;
+        System.out.println(sum + " " + average);
     }
 }
--- java/src/Student.java
+++ java/src/Student.java
@@ -1,15 +1,62 @@
 public class Student {
-    String name;
-    int age;
-    String address;
+    private String name;
+    private int age;
+    private String address;
+    private String personalNum;
 
-    public Student() { //기본 생성자
+    public Student() { // 기본 생성자
 
     }
 
-    public Student(String name, int age, String address) {
+    public Student(String name, int age, String address, String personalNum) { //일반 생성자
         this.name = name;
         this.age = age;
         this.address = address;
+        this.personalNum = personalNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;     // getter - 리턴 타입 int (호출한 사람에게 주는 것)
+    }
+
+    public void setAge(int age) {
+        this.age = age;  // setter - 매개변수 int age
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPersonalNum() {
+        return personalNum;
+    }
+
+
+
+    public void displayName() { // 메소드
+        System.out.println(this.name);
+    }
+    public void displayAge() {
+        System.out.println(this.age);
+    }
+
+    public void displayAddress(){
+        System.out.println(this.address);
+    }
+
+    public void displayAll() {
+        System.out.println(this.name + " " + this.age + " " + this.address);
     }
 }
