a910bdd8e8b5fc22480baa3da2fdda57b8d75f10
haeun01 <cjswldhkdpf@naver.com>
2024-03-25 17:25:27 +0900

    Lasttime commit

--- java/src/Java0325/Example.java
+++ java/src/Java0325/Example.java
@@ -13,7 +13,7 @@ public class Example {
 
         // 두번째 미션
         // 아래 메서드를 완성하시오
-        // 주어진 배열을 이용하여 Students타입의 배열을 만들고 for문으로 출력해야함
+        // 주어진 배열을 이용하여 Students타입의 배열을 만들고 for문으로 출력해야함 (복습하기)
         createStudentsInfo();
     }
 
@@ -21,8 +21,15 @@ public class Example {
         String[] names = {"Steve", "tom", "Laura", "Susie"};
         String[] departments = {"Physics", "Computer", "English", "Law"};
         int[] studentsIds = {10001, 10002, 10003, 10004};
-
-
-
+        // 데이터베이스의 raw data(가공하지 않은 순수 데이터)를
+        // Student 객체형태로 만들고 이를 배열에 담는다.
+        Students[] students = new Students[names.length];
+        for(int i = 0; i < names.length; i++) {
+            //students[i] = new Students(names[i], departments[i], studentsIds[i]);
+            students[i].setName(names[i]);
+            students[i].setDepartment(departments[i]);
+            students[i].setStudentId(studentsIds[i]);
+            System.out.println(students[i].toString());
+        }
     }
 }
--- java/src/Java0325/Students.java
+++ java/src/Java0325/Students.java
@@ -5,30 +5,42 @@ public class Students {
     private String department;
     private int studentId;
 
-    public Students (){
+    public Students(){
+
+    }
+    public Students (String name,String department, int studentId){
+        this.name = name;
+        this.department = department;
+        this.studentId = studentId;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getDepartment() {
+
         return department;
     }
 
     public void setDepartment(String department) {
+
         this.department = department;
     }
 
     public int getStudentId() {
+
         return studentId;
     }
 
     public void setStudentId(int studentId) {
+
         this.studentId = studentId;
     }
     @Override
