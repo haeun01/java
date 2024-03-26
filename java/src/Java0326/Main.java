85940f766fafdfa376d844ee9c66443aa1977442
haeun01 <cjswldhkdpf@naver.com>
2024-03-26 16:45:20 +0900

    Lasttime commit

--- java/src/Java0326/Main.java
+++ java/src/Java0326/Main.java
@@ -9,6 +9,36 @@ public class Main {
         System.out.println(A.a);
         System.out.println(B.a);
 
-        
+        A a2 = new A(){
+            @Override
+            public void abc() {
+                System.out.println("나는 익명 이너클래스");
+            }
+        };
+        a2.abc();
+
+        // <인터페이스 생성 방법1>
+        // 자식클래스(ChildA)를 선언하고 해당 인터페이스를 상속하게 하면
+        // 아래와 같이 동일한 코드 내용(=메서드 오버라이딩 내용)을 계속해서 사용할 수 있음
+        ChildA c2 = new ChildA();
+        ChildA c3 = new ChildA();
+        ChildA c4 = new ChildA();
+
+        // <인터페이스 생성 방법2>
+        // 아래와 같이 익명 이너 클래스 형식으로 생성하면,
+        // 방법1에서와 같이 재사용할 수 있는 클래스 이름이 없으므로
+        // 동일한 코드 내용(=메서드 오버라이딩 내용)을 매번 복사+붙여넣기 해줘야 함
+        A a3 = new A() {
+            @Override
+            public void abc() {
+                System.out.println("나는 익명 이너클래스");
+            }
+        };
+        A a4 = new A() {
+            @Override
+            public void abc() {
+                System.out.println("나는 익명 이너클래스");
+            }
+        };
     }
 }
---
+++ java/src/Java0326Example1/Circle.java
@@ -0,0 +1,23 @@
+package Java0326Example1;
+
+public class Circle implements Shape {
+    private double radius;  //반지름
+    private final double PI = 3.14159265358979; //상수의 경우, 대문자 사용.
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return PI * (radius * radius);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * radius * PI;
+    }
+}
